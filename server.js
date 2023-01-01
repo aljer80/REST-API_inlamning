ed1e50c4b371a8be92bcf68d235bda36832d07cd
aljer80 <jernberg.alexandra@gmail.com>
2023-01-01 23:35:28 +0100

    G klart

--- server.js
+++ server.js
@@ -22,15 +22,15 @@ app.post("/api/players", (req, res) => {
 
     const data = fs.readFileSync("players.json");
     const players = JSON.parse(data);
-    //let newPlayer = { "id": "players.length" + 1, "side": "backhand", "gender": "female", "level": "intermediated"}
     const player = req.body;
+    player.id = players.length +1;
     players.push(player);
 
     fs.writeFile("players.json", JSON.stringify(players, null, 2), function(err){
         if (err) {
             res.status(400).send();
         } else{
-             res.status(201).json(req.body);
+             res.status(200).json(player);         //skickar tillbaka det skapade objektet
         }
     });
 });
@@ -38,13 +38,49 @@ app.post("/api/players", (req, res) => {
 
 //PUT-anrop
 app.put("/api/players/:id", (req, res) => {
-    res.status(200).json(req.body);
-  });
+    const data = fs.readFileSync("players.json");
+    const players = JSON.parse(data);
+    const player = req.body;        //kommer från frontEnd
+    const existingPlayer = players.find(player => player.id === parseInt(req.params.id));
+    existingPlayer.id = player.id;  //vill ha med id, men skulle programmera så att inte vem som helst kan ändra (i fortsättningen)
+    existingPlayer.side = player.side;
+    existingPlayer.gender = player.gender;
+    existingPlayer.level = player.level;
+
+    if (!player) {
+        res.status(400).send("Bad request.");
+    } else {
+        fs.writeFile("players.json", JSON.stringify(players, null, 2), function(err){
+            if (err) {
+                res.status(500).send();
+            } else {
+                 res.status(200).send(existingPlayer);
+            }
+        });
+    }
+});
+
 
   //DELETE-anrop
 app.delete("/api/players/:id", (req, res) => {
-    res.status(200).json(req.body);
-  });
+    const data = fs.readFileSync("players.json");
+    const players = JSON.parse(data);
+    const id = req.params.id; 
+
+    if (players.find(player => player.id === parseInt(req.params.id))) {
+    players.splice(id-1, 1);
+    fs.writeFile("players.json", JSON.stringify(players, null, 2), function(err){
+        if (err) {
+            res.status(500).send();
+        } else {
+            res.status(200).send("Player deleted");
+        }
+    });
+    } else {
+        res.status(404).send('The player with the given ID was not found.');
+    }
+});
+
 
 
 //För VG
